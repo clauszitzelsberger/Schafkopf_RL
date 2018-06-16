e779061fa633bde7e7f54e732d7b6f90e4e1b50b
clauszitzelsberger <40275652+clauszitzelsberger@users.noreply.github.com>
2018-06-16 20:37:04 +0200

    Update script.py

--- script.py
+++ script.py
@@ -5,11 +5,13 @@ Created on Sun Jun 10 15:51:54 2018
 @author: claus
 """
 
-from rules import Rules
-from state_overall import state_overall
-from state_player import state_player
+#from rules import Rules
+#from state_overall import state_overall
+#from state_player import state_player
 from playing_schafkopf import playing_schafkopf
 
 p = playing_schafkopf()
+
+p.populate_memory()
 
 p.training()

b2fd20c7f416980c9ecd321ed3e9f30ea1dde6e0
clauszitzelsberger <40275652+clauszitzelsberger@users.noreply.github.com>
2018-06-16 20:33:56 +0200

    Update playing_schafkopf.py

--- playing_schafkopf.py
+++ playing_schafkopf.py
@@ -7,20 +7,15 @@ import numpy as np
 import random
 import copy
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 class playing_schafkopf():
     def __init__(self):
         self.rules = Rules()
-        self.state_overall = state_overall()
 
-
-
-        self.QNetwork = QNetwork()
-        #self.Memory = Memory()
-
-        self.train_episodes = 1000          # max number of episodes to learn from
-        self.max_steps = 200                # max steps in an episode
-        self.gamma = 0.99                   # future reward discount
+        self.train_episodes = 10000          # max number of episodes to learn from
+        #self.max_steps = 200                # max steps in an episode
+        self.gamma = 1                       # future reward discount
 
         # Exploration parameters
         self.explore_start = 1.0            # exploration probability at start
@@ -29,12 +24,19 @@ class playing_schafkopf():
 
         # Network parameters
         self.hidden_size = 64               # number of units in each Q-network hidden layer
-        self.learning_rate = 0.0001         # Q-network learning rate
+        self.learning_rate = 0.00001         # Q-network learning rate
 
         # Memory parameters
         self.memory_size = 10000            # memory capacity
         self.batch_size = 20                # experience mini-batch size
         self.pretrain_length = self.batch_size   # number experiences to pretrain the memory
+
+        tf.reset_default_graph()
+        self.QNetwork = QNetwork(name='main',
+                                 hidden_size=self.hidden_size,
+                                 learning_rate=self.learning_rate)
+
+        self.memory = Memory(max_size=self.memory_size)
 
     def reset_epsiode(self):
         player0 = state_player()
@@ -52,14 +54,20 @@ class playing_schafkopf():
                         player2,
                         player3]
 
+        self.state_overall = state_overall()
+
     def dealing(self):
         dealed_cards = self.rules.deal_cards()
-        print(dealed_cards)
+        #print(dealed_cards)
         for i in range(len(self.players)):
             self.players[i].state_player['dealed_cards']=dealed_cards[i]
             self.players[i].state_player['remaining_cards']=dealed_cards[i]
 
-    def new_game_to_choose(self):
+    def new_game_to_choose(self, random_selection=True, sess=None):
+        """
+        random_selection: True, game selected radom,
+            False, game selected by Q-Net
+        """
 
         first_player = self.state_overall.state_overall['first_player']
 
@@ -72,17 +80,31 @@ class playing_schafkopf():
         for i in range(len(players)):
             possible_games = players[i].\
                 get_possible_games_to_play(self.state_overall)
-            selected_game = random.choice(possible_games)
-            if selected_game != None:
+
+            if random_selection:
+                selected_game = random.choice(possible_games)
+            else:
+                dealed_cards = players[i].state_player['dealed_cards']
+                state = [self.rules.get_index(card, 'card') for card in dealed_cards]
+                state = np.array(state)
+                feed = {self.QNetwork.inputs_: state.reshape((1, *state.shape))}
+                Qs = sess.run(self.QNetwork.output, feed_dict=feed)
+                Qs = Qs[0].tolist()
+                possible_actions = [self.rules.get_index(p_g, 'game') for p_g in possible_games]
+                Qs_subset = [i for i in Qs if Qs.index(i) in possible_actions]
+                action = np.argmax(Qs_subset)
+                selected_game = self.rules.games[action]
+
+            if selected_game != [None, None]:
                 self.state_overall.state_overall['game']=selected_game
-                self.state_overall.state_overall['game_player']=i
+                self.state_overall.state_overall['game_player']=(first_player+i)%4
                 break
 
     def play(self):
 
         trick_nr = self.state_overall.state_overall['trick_number']
 
-        # oder players
+        # order of players
         first_player = self.state_overall.state_overall['first_player']
 
         players = [self.players[first_player],
@@ -94,13 +116,13 @@ class playing_schafkopf():
             possible_cards = players[i].\
                 get_possible_cards_to_play(self.state_overall)
             selected_card = random.choice(possible_cards)
-            print('Possible Cards: {}'.format(possible_cards))
-            print('Selected Cards: {}'.format(selected_card))
+            #print('Possible Cards: {}'.format(possible_cards))
+            #print('Selected Cards: {}'.format(selected_card))
 
             # Update states for next player
             remaining_cards=copy.copy(players[i].state_player['remaining_cards'])
             remaining_cards.remove(selected_card)
-            remaining_cards.append([None, None])
+            #remaining_cards.append([None, None])
             players[i].state_player['remaining_cards'] = remaining_cards
             #players[i].state_player['remaining_cards'].append([None, None])
             self.state_overall.state_overall['course_of_game'][trick_nr][(first_player+i)%4] = \
@@ -116,25 +138,28 @@ class playing_schafkopf():
             self.state_overall.get_score()
         self.state_overall.state_overall['trick_number'] += 1
 
-    def populate_memory(self.):
-        memory = Memory(max_size=self.memory_size)
+    def populate_memory(self):
+
 
         # Make random actions and store experiences
         for i in range(self.pretrain_length):
             self.reset_epsiode()
             self.dealing()
-            new_game_to_choose()
+            self.new_game_to_choose()
 
             # Action
             game = self.state_overall.state_overall['game']
-            action = self.rule.get_index(game, 'game')
+            action = self.rules.get_index(game, 'game')
 
             # State
-            game_player = self.state_overall.state_overall['game_player']
-            state = self.players[game_player]['dealed_cards']
-            state = [self.rule.get_index(card, 'card') for card in state]
+            if game == [None, None]:
+                game_player = 0
+            else:
+                game_player = self.state_overall.state_overall['game_player']
+            dealed_cards = self.players[game_player].state_player['dealed_cards']
+            state = [self.rules.get_index(card, 'card') for card in dealed_cards]
 
-            if self.state_overall.state_overall['game'] != [None, None]:
+            if game != [None, None]:
                 while self.state_overall.state_overall['trick_number'] < 8:
                     self.play()
                 rewards = self.state_overall.get_reward_list()
@@ -143,18 +168,99 @@ class playing_schafkopf():
 
             # Reward
             reward = rewards[game_player]
-            memory.add(state, action, reward)
+            self.memory.add((state, action, reward))
+        #print(self.memory.sample(self.pretrain_length))
 
 
     def training(self):
-        self.reset_epsiode()
-        self.dealing()
-        self.new_game_to_choose()
-        if self.state_overall.state_overall['game'] != [None, None]:
-            while self.state_overall.state_overall['trick_number'] < 8:
-                self.play()
-            rewards = self.state_overall.get_reward_list()
-            print(rewards)
-            return rewards
-        return [0,0,0,0] #no game, reward is 0 for every player
-        #__init__()
+
+        saver = tf.train.Saver()
+        reward_list_100 = []
+        total_reward=0
+        with tf.Session() as sess:
+            # Initialize variables
+            sess.run(tf.global_variables_initializer())
+
+            for e in range(1, self.train_episodes):
+                self.reset_epsiode()
+                self.dealing()
+                #total_reward = 0
+                #t = 0
+
+                # Explore or Exploit
+                explore_p = self.explore_stop + \
+                    (self.explore_start - self.explore_stop)*np.exp(-self.decay_rate*1000*e)
+                if explore_p > np.random.rand():
+                    # Make a random action
+                    self.new_game_to_choose()
+                else:
+                    # Get action from Q-network
+                    self.new_game_to_choose(False, sess)
+
+                # Action
+                game = self.state_overall.state_overall['game']
+                action = self.rules.get_index(game, 'game')
+
+                # State
+                if game == [None, None]:
+                    game_player = 0
+                else:
+                    game_player = self.state_overall.state_overall['game_player']
+                dealed_cards = self.players[game_player].state_player['dealed_cards']
+                state = [self.rules.get_index(card, 'card') for card in dealed_cards]
+
+                if self.state_overall.state_overall['game'] != [None, None]:
+                    while self.state_overall.state_overall['trick_number'] < 8:
+                        self.play()
+                    rewards = self.state_overall.get_reward_list()
+                else:
+                    rewards = [0,0,0,0]
+
+                # Reward
+                reward = rewards[game_player]
+                self.memory.add((state, action, reward))
+
+
+
+
+                # Sample mini-batch from memory
+                batch = self.memory.sample(self.batch_size)
+                states = np.array([each[0] for each in batch])
+                actions = np.array([each[1] for each in batch])
+                rewards = np.array([each[2] for each in batch])
+
+                # Train network
+                target_Qs = sess.run(self.QNetwork.output,
+                                    feed_dict={self.QNetwork.inputs_: states})
+
+                targets = rewards + self.gamma * np.max(target_Qs, axis=1)
+
+                loss, _ = sess.run([self.QNetwork.loss, self.QNetwork.opt],
+                                    feed_dict={self.QNetwork.inputs_: states,
+                                               self.QNetwork.targetQs_: targets,
+                                               self.QNetwork.actions_: actions})
+
+                total_reward+=reward
+                if e%100==0:
+                    print('Episode: {}'.format(e),
+                          'Total reward: {}'.format(reward),
+                          'Training loss: {:.4f}'.format(loss))
+                    reward_list_100.append(total_reward)
+                    total_reward=0
+
+
+
+            #print('Total Reward: {}'.format(total_reward))
+            self.plot_reward(reward_list_100)
+
+
+            saver.save(sess, "checkpoints/schafkopf.ckpt")
+
+    def plot_reward(self, reward_list):
+        x = range(len(reward_list))
+        y = reward_list
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.set(xlabel='100 epochs', ylabel='total reward in 100 epochs',
+               title='Reward ~ epochs')
+        plt.show()

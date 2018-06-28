6659a6d527ed4ee5d25161bf0837b72cb270d95f
clauszitzelsberger <40275652+clauszitzelsberger@users.noreply.github.com>
2018-06-28 21:40:37 +0200

    Update script.py

--- script.py
+++ script.py
@@ -8,10 +8,13 @@ Created on Sun Jun 10 15:51:54 2018
 #from rules import Rules
 #from state_overall import state_overall
 #from state_player import state_player
-from playing_schafkopf import playing_schafkopf
+from train_select_game import train_select_game
+from train_select_cards import train_select_cards
 
-p = playing_schafkopf()
 
-p.populate_memory()
+#t = train_select_game()
+t = train_select_cards()
 
-p.training()
+t.populate_memory()
+
+t.training()

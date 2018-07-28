9597305586f53cde08d99f1b6c0e2e01c820a418
clauszitzelsberger <40275652+clauszitzelsberger@users.noreply.github.com>
2018-07-28 12:13:11 +0200

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -10,7 +10,7 @@ def plot_reward(reward_list, reward_list2,
         y4 = reward_list4
         plt.figure(figsize=(20,6))
         fig, ax = plt.subplots()
-        ax.plot(x, y, 'black', alpha=.5, label='player 0 (RL bot)')
+        ax.plot(x, y, 'black', alpha=.5, linewidth=5, label='player 0 (RL bot)')
         ax.plot(x, y2, 'red', alpha=.5, label='player 1 (random)')
         ax.plot(x, y3, 'yellow', alpha=.5, label='player 2 (random)')
         ax.plot(x, y4, 'orange', alpha=.5, label='player 3 (random)')
@@ -19,11 +19,11 @@ def plot_reward(reward_list, reward_list2,
         ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
         plt.show()
 
-def plot_loss(loss_list):
+def plot_loss(loss_list, loss_type):
     x = range(len(loss_list))
     y = loss_list
     fig, ax = plt.subplots()
     ax.plot(x, y)
-    ax.set(xlabel='epochs x100', ylabel='avg loss', title='Loss ~ epochs')
+    ax.set(xlabel='epochs x100', ylabel='avg loss', title='{} Loss ~ epochs'.format(loss_type))
     plt.show()
 

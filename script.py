0a4ef41aa39a0d0219529ebe1dd64bec0cb1fb6f
clauszitzelsberger <40275652+clauszitzelsberger@users.noreply.github.com>
2018-07-28 12:14:24 +0200

    Update script.py

--- script.py
+++ script.py
@@ -10,11 +10,17 @@ Created on Sun Jun 10 15:51:54 2018
 #from state_player import state_player
 from train_select_game import train_select_game
 from train_select_cards import train_select_cards
-
+from train_select_both import train_select_both
 
 #t = train_select_game()
-t = train_select_cards()
+#t = train_select_cards()
+t = train_select_both()
 
-t.populate_memory()
+#t.populate_memory()
 
+#t.training()
+#t.apply_model()
+
+t.populate_memoryC()
+t.populate_memoryG()
 t.training()

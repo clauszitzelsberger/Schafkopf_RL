2fecb69729eaf9872a19baac12c5202341741eb4
clauszitzelsberger <40275652+clauszitzelsberger@users.noreply.github.com>
2018-10-14 14:06:40 +0200

    Update interface_to_states.py

--- interface_to_states.py
+++ interface_to_states.py
@@ -90,13 +90,14 @@ class interface_to_states():
     def return_state_select_card(self, player_id):
         """
         returns complete state space for a player given his id
-            state_size: 1086
+            state_size: 1086 | 225
             - state_overall:
+                - player_id:4
                 - game: 9
                 - game_player: 4
                 - first_player: 4
                 - trick_nr: 8
-                - course_of_game: 32*32=1024
+                - course_of_game: 32*32=1024 or other options + current trick
                 - davongelaufen: 1
                 - scores: 4
             - state_player:
@@ -105,6 +106,8 @@ class interface_to_states():
         state_overall = self.return_state_overall()
         state_player = self.return_state_player(player_id)
         state_list =[]
+
+        state_list.extend(self.rules.get_one_hot([player_id],4))
 
         game = state_overall['game']
         state_list.extend(self.rules.get_one_hot_games([self.rules.get_index(game, 'game')]))
@@ -124,12 +127,12 @@ class interface_to_states():
         course_of_game = course_of_game.tolist()
         
         # Option 1: exact course of game as state
-        for card in course_of_game:
-            if card == [None, None]:
-                state_list.extend([0]*32)
-            else:
-                state_list.extend(self.rules.get_one_hot_cards([self.rules.get_index(card, 'card')]))
-        state_list.extend([int(state_overall['davongelaufen'])])
+        #for card in course_of_game:
+        #    if card == [None, None]:
+        #        state_list.extend([0]*32)
+        #    else:
+        #        state_list.extend(self.rules.get_one_hot_cards([self.rules.get_index(card, 'card')]))
+        #state_list.extend([int(state_overall['davongelaufen'])])
 
         # Option 2: cards already played
         #played_cards = [card for card in course_of_game if card != [None, None]]
@@ -142,18 +145,18 @@ class interface_to_states():
         #state_list.extend(self.rules.get_one_hot_cards(cards_in_trick_indexed))
 
         # Option 3: cards already played by each player
-        #for p in [0,1,2,3]:
-        #    # Alocate cards to players
-        #    played_cards = [item for i, item in enumerate(course_of_game) if (i-p)%4==0]
-        #    played_cards = [card for card in course_of_game if card != [None, None]]
-        #    played_cards_indexed = [self.rules.get_index(card, 'card') for card in played_cards]
-        #    state_list.extend(self.rules.get_one_hot_cards(played_cards_indexed))
+        for p in [0,1,2,3]:
+            # Alocate cards to players
+            played_cards = [item for i, item in enumerate(course_of_game) if (i-p)%4==0]
+            played_cards = [card for card in course_of_game if card != [None, None]]
+            played_cards_indexed = [self.rules.get_index(card, 'card') for card in played_cards]
+            state_list.extend(self.rules.get_one_hot_cards(played_cards_indexed))
 
         # Cards played in this trick
-        #cards_in_trick = state_overall['course_of_game'][trick_nr]
-        #cards_in_trick = [card for card in cards_in_trick if card != [None, None]]
-        #cards_in_trick_indexed = [self.rules.get_index(card, 'card') for card in cards_in_trick]
-        #state_list.extend(self.rules.get_one_hot_cards(cards_in_trick_indexed))
+        cards_in_trick = state_overall['course_of_game'][trick_nr]
+        cards_in_trick = [card for card in cards_in_trick if card != [None, None]]
+        cards_in_trick_indexed = [self.rules.get_index(card, 'card') for card in cards_in_trick]
+        state_list.extend(self.rules.get_one_hot_cards(cards_in_trick_indexed))
 
 
         state_list.extend([s/120 for s in state_overall['scores']])
@@ -163,3 +166,53 @@ class interface_to_states():
         state_list.extend(self.rules.get_one_hot_cards(remaining_cards_indexed))
 
         return state_list
+
+    def return_state_select_game(self, player_id):
+        """
+        return complete state for a player given his id:
+        States space to be a vector:
+        - Number of cards for every color within [7,9]: 4
+        - -"- [Koenig, 10]: 4
+        - Boolean if Sau is available for every color: 4
+        - Number of Unter: 4
+        - Nummber of Ober: 4
+        """
+        dealed_cards = self.return_state_player(player_id)['dealed_cards']
+
+        state_list=[]
+
+        colors=np.arange(0,4)
+
+        # [7,9]
+        for color in colors:
+            number_cards=0
+            for number in np.arange(0,3):
+                number_cards += len(self.rules.get_specific_cards(dealed_cards, [color, number]))
+            number_cards = number_cards/3.0
+            state_list.append(number_cards)
+
+        # [Koenig, 10]
+        for color in colors:
+            nummber_cards=0
+            for number in [3,6]:
+                number_cards += len(self.rules.get_specific_cards(dealed_cards, [color, number]))
+            number_cards = number_cards/2.0
+            state_list.append(number_cards)
+
+        # Sau
+        for color in colors:
+            state_list.append(len(self.rules.get_specific_cards(dealed_cards, [color, 7]))/4.0)
+
+        # Unter
+        for color in colors:
+            state_list.append(len(self.rules.get_specific_cards(dealed_cards, [color, 4]))/4.0)
+
+        # Ober
+        for color in colors:
+            state_list.append(len(self.rules.get_specific_cards(dealed_cards, [color, 5]))/4.0)
+            
+        return state_list
+
+
+
+
